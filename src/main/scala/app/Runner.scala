fdf838d0edf9d723bc47966c24bc9f537a96e94a
dmitri.iourinski <d.iourinski@gmail.com>
2018-03-02 23:12:02 +0700

    actually adding files to the repo skeleton

--- src/main/scala/app/Runner.scala
+++ src/main/scala/app/Runner.scala
@@ -1,6 +1,9 @@
 package app
 
 object Runner {
+  // at the moment the script can only parse poker games either from a file of interactively from command line
+  // this can be easily extended
+
   def main(args: Array[String]) {
     val parser = new InputProcessor()
     println("Enter the list of cards separated by a space (single or multiple), empty line ends the script: ")
